c57a4425c2d4795af43c1a3db7d88c39a99a0977
Andrew Elliott <andrewcaelliott@gmail.com>
2016-01-11 12:40:28 +0000

    #fish

--- blog/views.py
+++ blog/views.py
@@ -74,7 +74,7 @@ def query_answer(request):
     numberQuery = NumberQuery(number=query["number"].value(), multiple=query["multiple"].value(), unit=query["unit"].value())
 
 
-    answer = {"quip":"Here's your answer"}
+    answer = {"quip":"Here's your query_answer"}
     references=[]
     if query["measure"].value()=="e":
         answer = {"quip":"It's a long, long way to ..."}
@@ -89,7 +89,7 @@ def query_answer(request):
             ('Length of an iPhone 6',"{times:20.2f} iPhone 6's end to end",'1 / {fraction:20.0f} as long as an iPhone 6'),
         ]
     elif query["measure"].value()=="c":
-        answer = {"quip":"There are plenty of fish in the sea"}
+        answer = {"quip":"How many fish in the ocean?"}
         references = [
             ('Population of World','{times:20.2f} for every person in the world','One for every {fraction:20.0f} people in the world'),
             ('Population of China','{times:20.2f} for every person in China','One for every {fraction:20.0f} people in China'),
@@ -103,7 +103,7 @@ def query_answer(request):
             ('GDP of United Kingdom','{times:20.2f} times the UK GDP','{percent:20.2f} percent of the UK GDP','A 1 /{fraction:20.0f} fraction of the UK GDP'),
         ] 
     elif query["measure"].value()=="d":
-        answer = {"quip":"How many years can a mountain exist"}
+        answer = {"quip":"How many years can a mountain exist?"}
         references = [
             ('age of the universe','{times:20.2f} times the age of the universe','{percent:20.2f} percent of the age of the universe','1 /{fraction:20.0f} of the age of the universe'),
             ('first modern humans','{times:20.2f} times the period since the emergence of the first modern humans','{percent:20.2f} percent of the time since the emergence of the first modern humans','1 /{fraction:20.0f} of the time since the emergence of the first modern humans'),
