a79aba1252473d3104fe615e85580d47c684a5c7
Andrew Elliott <andrewcaelliott@gmail.com>
2017-02-19 20:21:45 +0000

    #Rosling Quotes

--- blog/config.py
+++ blog/config.py
@@ -243,7 +243,9 @@ quotes = (
         '"You don\'t have to be a mathematician to have a feel for numbers." John Forbes Nash, Jr.',
         '"Numbers are the musical notes with which the symphony of the universe is written" Adam Spencer',
         '"The simplest schoolboy is now familiar with facts for which Archimedes would have sacrificed his life." Ernest Renan',
-
+        '"Facts are better than myths, especially for understanding the world." Hans Rosling',
+        '"There\'s about one sword-swallower per 2 to 4 million persons in each country". Hans Rosling',
+        '"My interest is not data, it\'s the world. And part of world development you can see in numbers. Others, like human rights, empowerment of women, it\'s very difficult to measure in numbers." Hans Rosling',
 
     )
 
