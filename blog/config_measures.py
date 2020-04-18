6825af74ff6ce5c78ffb5b7804eea08a529a6ef9
Andrew Elliott <andrewcaelliott@gmail.com>
2020-04-19 00:19:33 +0100

    count.deaths

--- blog/config_measures.py
+++ blog/config_measures.py
@@ -1,4 +1,4 @@
 
 neatRatioLimits = {"extent":10000, "extent.hor":10000, "extent.ver":10000, "mass":20000, "duration":10000, 
-                    "duration.age":10000, "duration.span":10000, "count":500, "count.pop":500, "amount":500, 
+                    "duration.age":10000, "duration.span":10000, "count":500, "count.pop":500, "count.deaths":500, "amount":500, 
                     "volume":10000, "capacity":10000, "area":10000, "energy":1000, "info":1000000, "speed":1000, "ex":10000000, "ma":20000, "du":10000, "am":500, "co":500, "en": 1000, "?": 1000, "ca":10000}
