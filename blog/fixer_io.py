74731558b37f972d81730dc4947c47c5fd8c5247
echocentral <andrewcaelliott+ec@gmail.com>
2022-06-29 12:12:03 +0100

    master convert currencies via EUR

--- blog/fixer_io.py
+++ blog/fixer_io.py
@@ -8,11 +8,11 @@ def getUSDConversionFactor(currency, year = None):
         version = "latest"
     else:
         version = year+"-12-31"
-    response = callHttpGET("http://data.fixer.io/api/",version,{"access_key":"c65acaa2ab282e6eb961788d69eacc69","symbols":currency,"base":"USD","callback":"?"}).text
+    response = callHttpGET("http://data.fixer.io/api/",version,{"access_key":"c65acaa2ab282e6eb961788d69eacc69","symbols":currency+',USD',"base":"EUR","callback":"?"}).text
     open_b = response.find("{")
     close_b = response.rfind("}")
     jsonResponse = json.loads(response[open_b:close_b+1])
-    return jsonResponse["rates"][currency]
+    return jsonResponse["rates"][currency]/jsonResponse["rates"]['USD']
 
 def convertToUSD(amount, currency, year = None):    
     if (currency=="USD"):
@@ -26,12 +26,12 @@ def getCurrencyConversionFactor(currency1, currency2, year = None):
         version = "latest"
     else:
         version = year+"-12-31"
-    response = callHttpGET("http://data.fixer.io/api/",version,{"access_key":"c65acaa2ab282e6eb961788d69eacc69","symbols":currency1,"base":currency2,"callback":"?"}).text
+    response = callHttpGET("http://data.fixer.io/api/",version,{"access_key":"c65acaa2ab282e6eb961788d69eacc69","symbols":currency1+','+currency2,"base":'EUR',"callback":"?"}).text
     open_b = response.find("{")
     close_b = response.rfind("}")
     jsonResponse = json.loads(response[open_b:close_b+1])
 #    jsonResponse = json.loads(response[2:-1])
-    return jsonResponse["rates"][currency1]
+    return jsonResponse["rates"][currency1]/jsonResponse["rates"][currency2]
 
 def convertToCurrency(amount, currency1, currency2, year = None):    
     if currency1.find("USD")>=0:
