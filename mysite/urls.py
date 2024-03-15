5f16885d611eab8988e75189aca965f6dae79bc6
echocentral <andrewcaelliott+ec@gmail.com>
2024-03-15 10:23:41 +0000

    Update urls.py
    cleaned urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog import urls as blog_urls
 
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
+    url(r'', include(blog_urls)),
 ]
